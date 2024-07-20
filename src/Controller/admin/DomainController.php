adfe3f41d5cbfc832e88d19a8d8c0e73b90a2265
moustafa_dj <moustafadj10@gmail.com>
2024-07-20 14:37:37 +0200

    20/07/2024

--- src/Controller/admin/DomainController.php
+++ src/Controller/admin/DomainController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller\admin;
 
+use App\Entity\Domain;
 use App\Form\DomainType;
 use App\Repository\DomainRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class DomainController extends AbstractController
 {
     protected DomainRepository $domain;
+    protected ValidatorInterface $validator;
+    protected EntityManagerInterface $manager;
 
-    public function  __construct(DomainRepository $domain) {
+    public function  __construct(
+        DomainRepository $domain,
+        ValidatorInterface $validator,
+        EntityManagerInterface $manager
+    ) {
         $this->domain = $domain;
+        $this->validator = $validator;
+        $this->manager = $manager;
     }
 
     #[Route('/manager/domain', name: 'app_admin_domain')]
@@ -35,8 +47,24 @@ class DomainController extends AbstractController
         ]);
     }
 
-    public function store(){
+    #[Route('/manager/domain/store',methods:['POST'] , name:'app_domain_store')]
+    public function store(Request $request){
 
+        $domain = new Domain();
+
+        $form = $this->createForm(DomainType::class , $domain);
+        
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $domain = $form->getData();
+
+            $this->manager->persist($domain);
+
+            $this->manager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_domain');
     }
 
     public function edit(){
--- src/Form/DomainType.php
+++ src/Form/DomainType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Domain;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,9 +15,21 @@ class DomainType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name' , TextType::class)
-            ->add('description')
-            ->add('submit',SubmitType::class)
+            ->add('name' , TextType::class , [
+                'attr' =>[
+                    'class'=>'form-control'
+                ]
+            ])
+            ->add('description' ,TextareaType::class, [
+                'attr' =>[
+                    'class'=>'form-control'
+                ]
+            ])
+            ->add('submit',SubmitType::class , [
+                'attr' =>[
+                    'class'=>'btn btn-primary'
+                ]
+            ])
         ;
     }
 
