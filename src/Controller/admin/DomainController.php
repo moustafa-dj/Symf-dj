05c860a807fee53632c16b9a9264c6d5a7d3a6ea
moustafa_dj <moustafadj10@gmail.com>
2024-07-22 14:53:20 +0200

    22/07/2024

--- src/Controller/admin/DomainController.php
+++ src/Controller/admin/DomainController.php
@@ -39,17 +39,8 @@ class DomainController extends AbstractController
     }
 
     #[Route('manager/domain/create' , name:'app_domain_create',methods:['GET','POST'])]
-    public function create()
+    public function create(Request $request)
     {
-        $form = $this->createForm(DomainType::class);
-        return $this->render('admin/domain/create.html.twig',[
-            'form' => $form
-        ]);
-    }
-
-    #[Route('/manager/domain/store',methods:['POST'] , name:'app_domain_store')]
-    public function store(Request $request){
-
         $domain = new Domain();
 
         $form = $this->createForm(DomainType::class , $domain);
@@ -57,25 +48,53 @@ class DomainController extends AbstractController
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
+
             $domain = $form->getData();
 
             $this->manager->persist($domain);
 
             $this->manager->flush();
+
+            return $this->redirectToRoute('app_admin_domain');
+
         }
 
+        return $this->render('admin/domain/create.html.twig',[
+            'form' => $form
+        ]);
+    }
+
+
+    #[Route('/manager/domain/update/{id}' , name:'app_domain_update' , methods:['GET','POST'])]
+    public function update(Request $request , Domain $domain){
+
+        $form = $this->createForm(DomainType::class,$domain);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $domain = $form->getData();
+
+            $this->manager->persist($domain);
+
+            $this->manager->flush();
+
+            return $this->redirectToRoute('app_admin_domain');
+        }
+        return $this->render('admin/domain/update.html.twig',[
+            'form'=>$form,
+            'domain' => $domain
+        ]);
+    }
+    #[Route('manager/domain/delete/{id}',name:'app_domain_delete' , methods:['GET'])]
+    public function destroy(Domain $domain){
+
+        $this->manager->remove($domain);
+
+        $this->manager->flush();
+
         return $this->redirectToRoute('app_admin_domain');
-    }
-
-    public function edit(){
-
-    }
-
-    public function update(){
-
-    }
-
-    public function destroy(){
 
     }
 }
